0f79a8674313bc6cd8fa265ef30754c073334711
HananHamidMoussa <Hamo031@student.kristiania.no>
2025-02-19 14:59:15 +0100

    App.jsx is set and ready.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <h1>Welcome to our project</h1>,
-);
+createRoot(document.getElementById("root")).render(<App />);
