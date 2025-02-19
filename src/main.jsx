13676fe82bc1a247f969dfa6ec02f48f982ba827
HananHamidMoussa <Hamo031@student.kristiania.no>
2025-02-19 13:15:15 +0100

    Install prettier as a test command

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(<h1>Welcome to our project</h1>);
+createRoot(document.getElementById("root")).render(
+  <h1>Welcome to our project</h1>,
+);
