333319d46f98f3d35465150ce339c12fee082b8c
Helena Simonsen <hesi016@student.kristiania.no>
2025-02-20 12:22:42 +0100

    updated map in app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import { Application } from "./modules/app/App";
+import { App } from "./modules/app/App";
 
-createRoot(document.getElementById("root")!).render(<Application />);
+createRoot(document.getElementById("root")!).render(<App />);
--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
@@ -6,34 +6,19 @@ import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
 
-import { Layer } from "ol/layer";
-
 useGeographic();
 
-const view = new View({ center: [10.8, 59.9], zoom: 10 });
-const map = new Map({ view });
+const map = new Map({
+  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
 
-function MapView() {
-  const mapRef = useRef<HTMLDivElement>(null);
+export function App() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
-    return () => map.setTarget(undefined);
   }, []);
-  return <div ref={mapRef}></div>;
-}
 
-export function Application() {
-  const [layers, setLayers] = useState<Layer[]>([
-    new TileLayer({ source: new OSM() }),
-  ]);
-  useEffect(() => map.setLayers(layers), [layers]);
-  return (
-    <>
-      <header>
-        <h1>Skoler i Norge</h1>
-      </header>
-      <nav></nav>
-      <MapView />
-    </>
-  );
+  return <div ref={mapRef}></div>;
 }
