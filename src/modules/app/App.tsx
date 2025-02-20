3bba9aec29fab7a605105e4127344ffd23d16ffc
Helena Simonsen <hesi016@student.kristiania.no>
2025-02-20 12:35:11 +0100

    Adding emergency shelters

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -5,12 +5,25 @@ import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
 
 useGeographic();
 
 const map = new Map({
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+
+    new VectorLayer({
+      source: new VectorSource({
+        url: "Arbeidskrav-2025/public/geojson/emergency-shelters.geojson",
+        format: new GeoJSON(),
+      }),
+    }),
+  ],
+
+  view: new View({ center: [15, 65.3], zoom: 5 }),
 });
 
 export function App() {
