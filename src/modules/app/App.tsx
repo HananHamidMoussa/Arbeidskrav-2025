bc1612a1888180735126fd719b268cc80f5459cf
Helena Simonsen <hesi016@student.kristiania.no>
2025-02-26 13:47:21 +0100

    Updated app.tsx

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,102 +1,89 @@
-import React, { useEffect, useRef } from "react";
-import { Feature, Map, MapBrowserEvent, View } from "ol";
+import React, { useEffect, useRef, useState } from "react";
+import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
+import "./App.css";
 
 import "ol/ol.css";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
-import { GeoJSON } from "ol/format";
-import { Fill, Stroke, Style, Text } from "ol/style";
-import CircleStyle from "ol/style/Circle";
-import { FeatureLike } from "ol/Feature";
+
+import { Layer } from "ol/layer";
+import { ShelterLayers } from "../layers/shelterLayer";
 
 useGeographic();
 
-function originalStyle(feature: FeatureLike) {
-  return new Style({
-    stroke: new Stroke({
-      color: "green",
-      width: 2,
-    }),
-    fill: new Fill({
-      color: "white",
-    }),
-    text: new Text({
-      text: feature.getProperties().navn,
-      fill: new Fill({ color: "green" }),
-      stroke: new Stroke({ color: "white", width: 2 }),
-    }),
-  });
-}
+const userSource = new VectorSource();
+const view = new View({ center: [10, 60], zoom: 7 });
+const map = new Map({ view });
 
-function hoverStyle(_feature: FeatureLike) {
-  return new Style({
-    fill: new Fill({ color: "rgba(19,119,1,0.44)" }),
-  });
-}
-
-const civilDefenseLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "Arbeidskrav-2025/public/geojson/civil-defence.geojson",
-    format: new GeoJSON(),
-  }),
-  style: originalStyle,
-});
-
-const emergencySheltersLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "Arbeidskrav-2025/public/geojson/emergency-shelters.geojson",
-    format: new GeoJSON(),
-  }),
-  style: new Style({
-    image: new CircleStyle({
-      radius: 3,
-      fill: new Fill({ color: "rgba(255, 0, 0, 0.7)" }),
-      stroke: new Stroke({ color: "rgba(0, 0, 0, 0.9)", width: 0.5 }),
-    }),
-  }),
-});
-
+/*
 const map = new Map({
-  layers: [
-    new TileLayer({ source: new OSM() }),
-    civilDefenseLayer,
-    emergencySheltersLayer,
-  ],
-  view: new View({ center: [10, 60], zoom: 7 }),
+ layers: [
+   new TileLayer({ source: new OSM() }),
+
+   new VectorLayer({
+     source: new VectorSource({
+       url: "Arbeidskrav-2025/public/geojson/civil-defence.geojson",
+       format: new GeoJSON(),
+     }),
+
+     style: (feature) =>
+       new Style({
+         stroke: new Stroke({
+           color: "green",
+           width: 2,
+         }),
+         fill: new Fill({
+           color: "white",
+         }),
+         text: new Text({
+           text: feature.getProperties().navn,
+           fill: new Fill({ color: "green" }),
+           stroke: new Stroke({ color: "white", width: 2 }),
+         }),
+       }),
+   }),
+
+   new VectorLayer({
+     source: new VectorSource({
+       url: "Arbeidskrav-2025/public/geojson/emergency-shelters.geojson",
+       format: new GeoJSON(),
+     }),
+     style: defaultStyle,
+   }),
+ ],
+
+
 });
 
-export function App() {
-  const mapRef = useRef<HTMLDivElement | null>(null);
-  const focusFeatures = useRef<Feature[]>([]);
+*/
 
-  function handlePointerMove(e: MapBrowserEvent<MouseEvent>) {
-    for (const feature of focusFeatures.current) {
-      feature.setStyle(originalStyle(feature));
-    }
-
-    const features = civilDefenseLayer
-      .getSource()!
-      .getFeaturesAtCoordinate(e.coordinate);
-
-    if (features.length === 0) {
-      focusFeatures.current = [];
-      return;
-    }
-
-    for (const feature of features) {
-      feature.setStyle(hoverStyle(feature));
-    }
-
-    focusFeatures.current = features;
-  }
-
+function MapView() {
+  const mapRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     map.setTarget(mapRef.current!);
-    map.on("pointermove", handlePointerMove);
+    return () => map.setTarget(undefined);
   }, []);
-
   return <div ref={mapRef}></div>;
+}
+
+export function App() {
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+    new VectorLayer({
+      source: userSource,
+    }),
+  ]);
+  useEffect(() => map.setLayers(layers), [layers]);
+  return (
+    <>
+      <header></header>
+      <nav>
+        <ShelterLayers setLayers={setLayers} map={map} />
+      </nav>
+      <MapView />
+    </>
+  );
 }
